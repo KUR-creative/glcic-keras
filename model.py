3f98323d206a0e3568121e16c75a3b622b80c1e5
kur <rhdnfka94@gmail.com>
2018-05-27 20:18:18 +0900

    extract init_model function

--- model.py
+++ model.py
@@ -23,46 +23,52 @@ MASK_SHAPE = (IMG_SIZE,IMG_SIZE,1)
 VAR_IMG_SHAPE = (None,None,3)
 VAR_MASK_SHAPE = (None,None,1)
 
-#def completion_model():
-complnet_inp = Input(shape=IMG_SHAPE, name='complnet_inp')
-holed_origins_inp = Input(shape=IMG_SHAPE, name='holed_origins_inp')
-masks_inp = Input(shape=MASK_SHAPE, name='masks_inp')
-
-complnet_out = completion_net(VAR_IMG_SHAPE)(complnet_inp)
-merged_out = Add()([holed_origins_inp, 
-                     Multiply()([complnet_out, 
-                                 masks_inp])])
-compl_model = Model([holed_origins_inp, 
-                     complnet_inp, 
-                     masks_inp], merged_out)
-compl_model.compile(loss='mse', optimizer=Adadelta())
-
-#def discrimination_model():
-origins_inp = Input(shape=IMG_SHAPE, name='origins_inp')
-crop_yxhw_inp = Input(shape=(4,), dtype=np.int32, name='yxhw_inp')
-local_cropped = merge([origins_inp,crop_yxhw_inp], mode=cropping, 
-                      output_shape=MASK_SHAPE, name='local_crop')
-discrim_out = discrimination_net(IMG_SHAPE,
-                                 LD_CROP_SHAPE)([origins_inp,
-                                                 local_cropped])
-discrim_model = Model([origins_inp,crop_yxhw_inp], discrim_out)
-discrim_model.compile(loss='binary_crossentropy', 
-                      optimizer=Adadelta(lr=0.01)) # good? lol
-                      #optimizer=Adam(lr=0.000001))
-discrim_model.summary()
-plot_model(discrim_model, to_file='D_model.png', show_shapes=True)
-                      
-#def joint_model():
-d_container = Container([origins_inp,crop_yxhw_inp], discrim_out,
-                        name='D_container')
-d_container.trainable = False
-joint_model = Model([holed_origins_inp,complnet_inp,masks_inp,
-                     crop_yxhw_inp],
-                    [merged_out,
-                     d_container([merged_out,crop_yxhw_inp])])
 
 alpha = 0.0004
-joint_model.compile(loss=['mse', 'binary_crossentropy'],
-                    loss_weights=[1.0, alpha], optimizer=Adadelta())
-joint_model.summary()
-plot_model(joint_model, to_file='joint_model.png', show_shapes=True)
+Dmodel_lr = 0.01
+
+def init_models():
+    #----------------------------- completion_model ----------------------------
+    holed_origins_inp = Input(shape=IMG_SHAPE, name='holed_origins_inp')
+    complnet_inp = Input(shape=IMG_SHAPE, name='complnet_inp')
+    masks_inp = Input(shape=MASK_SHAPE, name='masks_inp')
+
+    complnet_out = completion_net(VAR_IMG_SHAPE)(complnet_inp)
+    merged_out = Add()([holed_origins_inp, 
+                         Multiply()([complnet_out, 
+                                     masks_inp])])
+    compl_model = Model([holed_origins_inp, complnet_inp, masks_inp], 
+                        merged_out)
+    compl_model.compile(loss='mse', optimizer=Adadelta())
+
+    #--------------------------- discrimination_model --------------------------
+    origins_inp = Input(shape=IMG_SHAPE, name='origins_inp')
+    crop_yxhw_inp = Input(shape=(4,), dtype=np.int32, name='yxhw_inp')
+
+    local_cropped = merge([origins_inp,crop_yxhw_inp], mode=cropping, 
+                          output_shape=MASK_SHAPE, name='local_crop')
+    discrim_out = discrimination_net(IMG_SHAPE, LD_CROP_SHAPE)([origins_inp, 
+                                                                local_cropped])
+    discrim_model = Model([origins_inp,crop_yxhw_inp], 
+                          discrim_out)
+    discrim_model.compile(loss='binary_crossentropy', 
+                          optimizer=Adadelta(lr=Dmodel_lr)) # good? lol
+                          #optimizer=Adam(lr=0.000001))
+    #discrim_model.summary()
+    #plot_model(discrim_model, to_file='D_model.png', show_shapes=True)
+                          
+    #------------------------------- joint_model -------------------------------
+    d_container = Container([origins_inp,crop_yxhw_inp], 
+                            discrim_out, name='D_container')
+    d_container.trainable = False
+    joint_model = Model([holed_origins_inp, complnet_inp, masks_inp, 
+                         crop_yxhw_inp],
+                        [merged_out, d_container([merged_out,crop_yxhw_inp])])
+
+    joint_model.compile(loss=['mse', 'binary_crossentropy'],
+                        loss_weights=[1.0, alpha], optimizer=Adadelta())
+
+    #joint_model.summary()
+    #plot_model(joint_model, to_file='joint_model.png', show_shapes=True)
+    return compl_model, discrim_model, joint_model
+
--- train.py
+++ train.py
@@ -1,19 +1,17 @@
-from model import compl_model, discrim_model, joint_model
-from model import BATCH_SIZE, IMG_SIZE, LD_CROP_SIZE, MAX_LEN, MIN_LEN
-
+from model import init_models, BATCH_SIZE, IMG_SIZE, LD_CROP_SIZE, MAX_LEN, MIN_LEN
 from data_generator import gen_batch
 from utils import ElapsedTimer
 
 import numpy as np
 import os, h5py
-#np.set_printoptions(threshold=np.nan, linewidth=np.nan)
 
+Cmodel, Dmodel, CDmodel = init_models()
 
 timer = ElapsedTimer('Total Training')
 from concurrent.futures import ProcessPoolExecutor
 #data_file = h5py.File('./data/mini_data.h5','r') 
 data_file = h5py.File('./data/data128.h5','r') 
-#--------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 data_arr = data_file['images']
 mean_pixel_value = data_file['mean_pixel_value'][()] / 255
 
@@ -31,7 +29,7 @@ valids = np.ones((BATCH_SIZE, 1))
 fakes = np.zeros((BATCH_SIZE, 1))
 for epoch in range(num_epoch):
     epoch_timer = ElapsedTimer('1 epoch training time')
-    #--------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------
     for batch in gen_batch(data_arr, BATCH_SIZE, IMG_SIZE, LD_CROP_SIZE,
                       MIN_LEN, MAX_LEN, mean_pixel_value):
         '''
@@ -40,44 +38,45 @@ for epoch in range(num_epoch):
         origins, complnet_inputs, masked_origins, masks, ld_crop_yxhws = batch
 
         #batch_timer = ElapsedTimer('1 batch training time')
-        #--------------------------------------------------------------------------------------
+        #-----------------------------------------------------------------------
         if epoch < tc:
-            mse_loss = compl_model.train_on_batch([masked_origins, complnet_inputs, masks],
+            mse_loss = Cmodel.train_on_batch([masked_origins, complnet_inputs, masks],
                                                   origins)
         else:
-            completed = compl_model.predict([masked_origins, complnet_inputs, masks])
-            d_loss_real = discrim_model.train_on_batch([origins,ld_crop_yxhws],valids)
-            d_loss_fake = discrim_model.train_on_batch([completed,ld_crop_yxhws],fakes)
+            completed = Cmodel.predict([masked_origins, complnet_inputs, masks])
+            d_loss_real = Dmodel.train_on_batch([origins,ld_crop_yxhws],valids)
+            d_loss_fake = Dmodel.train_on_batch([completed,ld_crop_yxhws],fakes)
             bce_d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
             if epoch >= tc + td:
-                joint_loss,mse,gan = joint_model.train_on_batch([masked_origins,complnet_inputs,masks,
+                joint_loss,mse,gan = CDmodel.train_on_batch([masked_origins,complnet_inputs,masks,
                                                                  ld_crop_yxhws],
                                                                 [origins, valids])
-        #--------------------------------------------------------------------------------------
+        #-----------------------------------------------------------------------
         #batch_timer.elapsed_time()
-    #--------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------
     epoch_timer.elapsed_time()
 
     if epoch < tc:
         print('epoch %d: [C mse loss: %e]' % (epoch, mse_loss))
     else:
-        print('epoch %d: [C mse loss: %e] [D bce loss: %e]' % (epoch, mse_loss, bce_d_loss))
+        print('epoch %d: [C mse loss: %e] [D bce loss: %e]' 
+                % (epoch, mse_loss, bce_d_loss))
         if epoch >= tc + td:
-            print('epoch %d: [joint loss: %e | mse loss: %e, gan loss: %e]' % (epoch, joint_loss, mse,gan))
+            print('epoch %d: [joint loss: %e | mse loss: %e, gan loss: %e]' 
+                    % (epoch, joint_loss, mse,gan))
 
             if epoch % save_interval == 0 or epoch == num_epoch - 1:
                 result_dir = 'output'
-                completed = compl_model.predict([masked_origins, 
-                                                 complnet_inputs, 
-                                                 masks])
+                completed = Cmodel.predict([masked_origins, 
+                                            complnet_inputs, 
+                                            masks])
                 np.save(os.path.join(result_dir,'I_O_GT__%d.npy' % epoch),
                         np.array([complnet_inputs,completed,origins]))
-                        # save predicted image of last batch in epoch.
-                compl_model.save_weights(os.path.join(result_dir, 
-                                                      "complnet_%d.h5" % epoch))
-                discrim_model.save_weights(os.path.join(result_dir, 
-                                                        "discrimnet_%d.h5" % epoch))
-#--------------------------------------------------------------------------------------
+                Cmodel.save_weights(os.path.join(result_dir, 
+                                                 "complnet_%d.h5" % epoch))
+                Dmodel.save_weights(os.path.join(result_dir, 
+                                                 "discrimnet_%d.h5" % epoch))
+#-------------------------------------------------------------------------------
 time_str = timer.elapsed_time()
 data_file.close()
 #import mailing
@@ -88,7 +87,7 @@ if __name__ == "__main__":
     main()
     timer.elapsed_time()
 
-    #plot_model(compl_model, to_file='mse_model.png', show_shapes=True)
+    #plot_model(Cmodel, to_file='mse_model.png', show_shapes=True)
     #model.summary()
 '''
 
