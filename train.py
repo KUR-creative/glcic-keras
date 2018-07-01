7bb80831fad6fec65b704ceb9b23959188f3e997
kur <rhdnfka94@gmail.com>
2018-07-02 00:16:43 +0900

    Add model sanity checking comment

--- train.py
+++ train.py
@@ -44,10 +44,61 @@ def save(Cmodel,Dmodel,batch, period,epoch,num_epoch, result_dir):
             os.path.join(result_dir, "discrimnet_%d.h5" % epoch)
         )
 
-Cmodel, Dmodel, CDmodel = init_models()
 
 from tqdm import tqdm
 def train(DATASET_NAME, NUM_EPOCH, Tc, Td, SAVE_INTERVAL, MAILING_ENABLED,learned_data_ratio):
+    Cmodel, Dmodel, CDmodel = init_models()
+    ''' model sanity checking
+    from keras.utils import plot_model
+    Cmodel.summary(); Dmodel.summary(); CDmodel.summary();
+    plot_model(Cmodel, to_file='Cmodel.png', show_shapes=True)
+    plot_model(Dmodel, to_file='Dmodel.png', show_shapes=True)
+    plot_model(CDmodel, to_file='CDmodel.png', show_shapes=True)
+    '''
+    data_file = h5py.File(DATASET_NAME,'r') 
+    #-------------------------------------------------------------------------------
+    data_arr = data_file['images'] # already preprocessed, float32.
+    mean_pixel_value = data_file['mean_pixel_value'][()] # value is float
+    learned_arr_len = int(data_arr.shape[0] * learned_data_ratio)
+    learned_arr_len = learned_arr_len - (learned_arr_len % BATCH_SIZE)#never use remainders..
+    print('data_arr shape: ', data_arr.shape)
+    print('length of data to learn: ', learned_arr_len)
+
+    timer = ElapsedTimer('Total Training')
+    #-------------------------------------------------------------------------------
+    for epoch in tqdm(range(NUM_EPOCH)):
+        #epoch_timer = ElapsedTimer()
+        #--------------------------------------------------------------------------
+        for batch in gen_batch(data_arr, BATCH_SIZE, IMG_SHAPE, LD_CROP_SIZE,
+                               HOLE_MIN_LEN, HOLE_MAX_LEN, mean_pixel_value,
+                               learned_arr_len):
+            if epoch < Tc:
+                mse_loss = trainC(Cmodel, batch, epoch)
+            else:
+                bce_d_loss = trainD(Cmodel, Dmodel, batch, epoch)
+                if epoch >= Tc + Td:
+                    joint_loss,mse,gan = trainC_in(CDmodel, batch, epoch)
+        #--------------------------------------------------------------------------
+        if epoch < Tc:
+            print('epoch {}: [C mse loss: {}]'.format(epoch, mse_loss), flush=True)#, end='')
+        else:
+            if epoch >= Tc + Td:
+                print('epoch {}: [joint loss: {} | mse loss: {}, gan loss: {}]'\
+                       .format(epoch, joint_loss, mse, gan), flush=True)#, end='')
+            else:
+                print('epoch {}: [D bce loss: {}]'.format(epoch, bce_d_loss), flush=True)#, end='')
+        #epoch_timer.elapsed_time()
+        #print()
+        save(Cmodel,Dmodel,batch, SAVE_INTERVAL,epoch,NUM_EPOCH, 'output')
+    #-------------------------------------------------------------------------------
+    time_str = timer.elapsed_time()
+    data_file.close()
+
+    if MAILING_ENABLED:
+        import mailing
+        mailing.send_mail_to_kur(time_str)
+
+def continued_train(DATASET_NAME, Dnet_name, Cnet_name,NUM_EPOCH, Tc, Td, SAVE_INTERVAL, MAILING_ENABLED,learned_data_ratio):
     data_file = h5py.File(DATASET_NAME,'r') 
     #-------------------------------------------------------------------------------
     data_arr = data_file['images'] # already preprocessed, float32.
